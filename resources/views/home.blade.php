f1e87a6d01a3bfa58fcf33110ce1940062cae3a5
Medina Marcos A <47308522+mamedinapixon@users.noreply.github.com>
2022-12-13 10:33:19 -0300

    Fix Ortografía

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,7 +31,7 @@
                             <div class="w-48 alert">
                                 <div>
                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="flex-shrink-0 w-6 h-6 stroke-info"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                  <span>En contracción</span>
+                                  <span>Se habilitará próximamente</span>
                                 </div>
                               </div>
                             <button class="btn btn-primary">Ingresar</button>
--- resources/views/livewire/planificacion/create.blade.php
+++ resources/views/livewire/planificacion/create.blade.php
@@ -60,11 +60,11 @@
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="flex-shrink-0 w-6 h-6 stroke-info"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
             <span>Comuníquese con Académica si no aparece la asignatura.</span>
           </div>
-        <div class="flex flex-row space-x-4 form-control">
+        <div class="flex flex-row space-x-4">
             <a class="max-w-xs btn" href="{{ route('planificacion.index') }}"  >
                 Volver
             </a>
-            <button class="btn btn-primary" wire:click="store">Crear planificaicon y continuar</button>
+            <button class="btn btn-primary" wire:click="store">Crear planificación y continuar</button>
         </div>
 
     </div>
