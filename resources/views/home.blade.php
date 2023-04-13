0b52b98b307e9bcbf1f07d9a90b61c86f1a3eae6
Medina Marcos A <47308522+mamedinapixon@users.noreply.github.com>
2023-04-13 12:17:37 -0300

    add menu memoria

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,13 +28,7 @@
                         <h2 class="card-title">Memoria individual</h2>
                         <p>Debe ser llenada por cada docente de la institución en forma individual</p>
                         <div class="items-center justify-between card-actions">
-                            <div class="w-48 alert">
-                                <div>
-                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="flex-shrink-0 w-6 h-6 stroke-info"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                  <span>Se habilitará próximamente</span>
-                                </div>
-                              </div>
-                            <button class="btn btn-primary">Ingresar</button>
+                            <a href="{{ route('memoria.index') }}" class="btn btn-primary">Ingresar</a>
                         </div>
                     </div>
                 </div>
