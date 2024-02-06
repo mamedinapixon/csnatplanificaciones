d7d7f7b3e25ae9c3ae76adac1256839e7f68880d
Medina Marcos A <47308522+mamedinapixon@users.noreply.github.com>
2024-02-06 11:08:57 -0300

    Update AsistenciaTable.php

--- app/Http/Livewire/Asistencia/AsistenciaTable.php
+++ app/Http/Livewire/Asistencia/AsistenciaTable.php
@@ -51,7 +51,7 @@ class AsistenciaTable extends DataTableComponent
                 ->sortable(),
             Column::make("Ingreso", "ingreso_at")
                 ->format(
-                    fn($value, $row, Column $column) => $row->ingreso_at->toDateString().' '.$row->ingreso_at->toTimeString()
+                    fn($value, $row, Column $column) => $row->ingreso_at->format('d/m/Y').' '.$row->ingreso_at->toTimeString()
                 )
                 ->sortable(),
             Column::make("Salida", "salida_at")
