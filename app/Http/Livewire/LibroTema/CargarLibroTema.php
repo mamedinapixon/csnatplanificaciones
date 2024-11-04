fde0ddd13f7fd1089791231584d922929ad4fe69
Medina Marcos A. <47308522+mamedinapixon@users.noreply.github.com>
2024-11-04 11:34:17 -0300

    update carga libro tema

--- app/Http/Livewire/LibroTema/CargarLibroTema.php
+++ app/Http/Livewire/LibroTema/CargarLibroTema.php
@@ -4,18 +4,24 @@ namespace App\Http\Livewire\LibroTema;
 
 use Livewire\Component;
 use App\Models\PeriodoLectivo;
+use App\Models\Planificacion;
 use Filament\Forms;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\DatePicker;
 
 
 
 class CargarLibroTema extends Component implements Forms\Contracts\HasForms
 {
     use Forms\Concerns\InteractsWithForms;
+
+    public $periodo_lectivo_id;
+    public $planificacion_id;
+    public $fecha;
 
     public function render()
     {
@@ -36,10 +42,29 @@ class CargarLibroTema extends Component implements Forms\Contracts\HasForms
                             return [$periodoLectivo->id => $label];
                         });
                 })
+                ->reactive()
                 ->label('Periodo Lectivo')
                 ->required(),
-            /*Select::make('authorId')
-                ->relationship('author', 'name', fn (Builder $query) => $query->withTrashed()),
+            Select::make('planificacion_id')
+                ->options(function (callable $get) {
+                    $periodo_lectivo_id = $get('periodo_lectivo_id');
+                    if ($periodo_lectivo_id) {
+                        return Planificacion::where('periodo_lectivo_id', $periodo_lectivo_id)
+                            ->get()
+                            ->mapWithKeys(function ($planificacion) {
+                                $label = $planificacion->materiaPlanEstudio->carrera->codigo_siu . ' - ' . $planificacion->materiaPlanEstudio->materia->nombre;
+                                return [$planificacion->id => $label];
+                            });
+                    }
+                    return [];
+                })
+                ->reactive()
+                ->label('Materia')
+                ->required(),
+            DatePicker::make('fecha')
+                ->displayFormat('d/m/Y')
+                ->required(),
+            /*
             TextInput::make('title')->required(),
             RichEditor::make('content'),*/
             // ...
