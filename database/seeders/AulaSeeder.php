aaa59ac09ed1197bd9e02a9665dcdcf3ac4f4c02
Medina Marcos A. <47308522+mamedinapixon@users.noreply.github.com>
2024-12-02 09:56:17 -0300

    Update AulaSeeder.php

--- database/seeders/AulaSeeder.php
+++ database/seeders/AulaSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Aula;
 
 class AulaSeeder extends Seeder
 {
@@ -13,6 +14,28 @@ class AulaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $aulas = [
+            'Aula 2',
+            'Aula 34',
+            'Aula 5',
+            'Aula 6',
+            'Aula 7',
+            'Aula 10',
+            'Aula 11',
+            'Anfiteatro',
+            'Sala de inforática',
+            'Sala de conferencias',
+            'Laboratorio química',
+            'Laboratorio de Suelo',
+            'Aula C',
+            'Aula D',
+            'SUM del Anexo',
+            'Aula Virla',
+            'Otra',
+        ];
+
+        foreach ($aulas as $aula) {
+            Aula::create(['nombre' => $aula]);
+        }
     }
 }
