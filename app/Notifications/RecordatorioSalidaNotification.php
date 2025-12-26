64b3025bbcf80ade9b6ac847271310a44532ad7a
mamedinapixon <mamedina.pixon@gmail.com>
2025-12-26 10:38:41 -0300

    Update RecordatorioSalidaNotification.php

--- app/Notifications/RecordatorioSalidaNotification.php
+++ app/Notifications/RecordatorioSalidaNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class RecordatorioSalidaNotification extends Notification
+class RecordatorioSalidaNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
