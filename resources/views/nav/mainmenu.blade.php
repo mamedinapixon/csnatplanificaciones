ae59a42cb96ba715d30e80e03a2a9d37f686e3dd
mamedinapixon <mamedina.pixon@gmail.com>
2026-03-05 11:49:16 -0300

    Remove conditional menu items for 'periodos lectivos', 'docentes', and 'usuarios' from main menu in nav view

--- resources/views/nav/mainmenu.blade.php
+++ resources/views/nav/mainmenu.blade.php
@@ -2,39 +2,6 @@
 @role('admin')
     <li><a href="{{ url('/admin') }}">Panel Admin</a></li>
 @endrole
-@can(['ver periodos lectivos', 'ver docentes'])
-    <li tabindex="0" class="m-0">
-        <details>
-            <summary style="padding-left: 0px; margin-left: -3px;">
-                Administración
-            </summary>
-            <ul tabindex="0"
-                class="p-2 bg-base-100 text-neutral">
-                @can('ver periodos lectivos')
-                    <li class="hover-bordered">
-                        <a class="w-full justify-between" href="{{ route('periodoLectivo.index') }}">
-                            Periodos Lectivos
-                        </a>
-                    </li>
-                @endcan
-                @can('ver docentes')
-                    <li class="hover-bordered">
-                        <a class="w-full justify-between" href="{{ route('docente.index') }}">
-                            Docentes
-                        </a>
-                    </li>
-                @endcan
-                @can('ver usuarios')
-                    <li class="hover-bordered">
-                        <a class="w-full justify-between" href="{{ route('user.index') }}">
-                            Usuarios
-                        </a>
-                    </li>
-                @endcan
-            </ul>
-        </details>
-    </li>
-@endcan
 @can('ver planificaciones')
     <li><a href="{{ route('planificacion.index') }}">Planificaciones</a></li>
 @else
